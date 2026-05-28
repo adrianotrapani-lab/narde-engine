63bdb77229143c8848dd8c3eecedff6c72f8b613
Your Name <you@example.com>
2026-05-28 14:44:04 +0700

    Add integration test for asset headers

---
+++ tests/test_assets_headers.py
@@ -0,0 +1,7 @@
+﻿import requests
+
+def test_assets_headers():
+    r = requests.get('http://127.0.0.1:8000/assets/static/js/board.js')
+    assert r.status_code == 200
+    assert 'ETag' in r.headers
+    assert r.headers.get('content-type','').startswith('text/javascript')
