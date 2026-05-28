a32acb9e83dbbce0d357a79547bd49f03b4f5b3e
Your Name <you@example.com>
2026-05-28 14:33:09 +0700

    Add integration test for assets and assets/static paths

---
+++ tests/test_static_paths.py
@@ -0,0 +1,5 @@
+﻿import requests
+
+def test_assets_paths():
+    assert requests.get('http://127.0.0.1:8000/assets/js/board.js').status_code == 200
+    assert requests.get('http://127.0.0.1:8000/assets/static/js/board.js').status_code == 200
